05d624f9cc011a52d2263a1caff35c7066f67b9d
chrisecmoffat <chris.ec.moffat@gmail.com>
2017-03-02 16:33:52 -0500

    Update BigBucks.java

--- BigBucks.java
+++ BigBucks.java
@@ -1,27 +1,28 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 class Processor implements Runnable {
 
-	public int id;
-	public int blockSize;
-	public int startBlock;
-	public int endBlock;
-	public long startThread;
-	public long endThread;
-	static Counter myCounter = new Counter(new ReentrantLock());
+	private int id;
+	private int startBlock;
+	private int blockSize;
+	private int endBlock;
+	private long startThread;
+	private long endThread;
+	private static Counter myStats = new Counter(new ReentrantLock());
 	
 	Lock lock = new ReentrantLock();
 	
 	public Processor(int id, int blockSize) {
-		this.id = id;
-		this.blockSize = blockSize;
+		this.setId(id);
+		this.setBlockSize(blockSize);
 		this.startBlock = (id * blockSize);
 		this.endBlock = ((id * blockSize) + blockSize);
 	}
@@ -32,36 +33,50 @@ class Processor implements Runnable {
 		
 		for (int i = startBlock; i < endBlock; i++) {	
 			
-			if (myCounter.checkStatus())
+			if (myStats.checkStatus())
 				break;
 			
 			String intStr = Integer.toBinaryString(i);
 			
-			// Palindrome algo, checks binary string odd or even
-			// Compares left and right side
+			// Palindrome algo, checks binary string odd or even, compares left and right side
 			if (intStr.length() % 2 == 0) {
 				String frontHalf = intStr.substring(0, (intStr.length() / 2));
 				String backHalf = intStr.substring((intStr.length() / 2), (intStr.length()));
 				
 				if ((frontHalf).equals(new StringBuilder(backHalf).reverse().toString())) {
-					myCounter.addCount();
-					myCounter.addList(Integer.parseInt(intStr, 2));
+					//myStats.addCount();
+					myStats.addList(Integer.parseInt(intStr, 2));
 				} 
 			} else {
 				String frontHalf = (intStr.substring(0, ((intStr.length() - 1) / 2)));
 				String backHalf = (intStr.substring(((intStr.length() + 1) / 2), (intStr.length())));	
 				
 				if ((frontHalf).equals(new StringBuilder(backHalf).reverse().toString())) {
-					myCounter.addCount();
-					myCounter.addList(Integer.parseInt(intStr, 2));
+					//myStats.addCount();
+					myStats.addList(Integer.parseInt(intStr, 2));
 				} 
 			}
 		}
 		endThread = System.currentTimeMillis();
-		myCounter.threadTime(endThread - startThread);
+		myStats.threadTime(endThread - startThread);
+	}
+
+	public int getBlockSize() {
+		return blockSize;
+	}
+
+	public void setBlockSize(int blockSize) {
+		this.blockSize = blockSize;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 }
-
 
 public class bigBucks {
 	
@@ -69,39 +84,39 @@ public class bigBucks {
 		
 		long startTime;
 		long endTime;
-		final int blockSize = 100000000;
-		final int poolSize = 8;
 		final int timeOut = 10;
-		final Counter myCounter = new Counter(new ReentrantLock());
+		final int poolSize = 8;
+		final int blockSize = 1000;
+		final Counter myStats = new Counter(new ReentrantLock());
 		
 		startTime = System.currentTimeMillis();
 		ExecutorService executor = Executors.newFixedThreadPool(poolSize);
 		
 		// Spins up 8 threads
-		for (int i = 0; i < poolSize; i++)
+		for (int i = 0; i < 8; i++)
 			executor.submit(new Processor(i, blockSize));
 		
 		executor.shutdown();
 		
 		try {
 			if (!executor.awaitTermination(timeOut, TimeUnit.SECONDS))
-				myCounter.terminateThread();
+				myStats.terminateThread();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 
-		Collections.sort(myCounter.getList());
+		Collections.sort(myStats.getList());
 		
 		endTime = System.currentTimeMillis();
 		
 		System.out.println("");
 		System.out.println("Palindromes:");
 		System.out.println("...");
-		for (int e : myCounter.getList()) 
-		    System.out.println(e + " binary: " + Integer.toBinaryString(e));
+		for (int i : myStats.getList()) 
+		    System.out.println(i + " binary: " + Integer.toBinaryString(i));
 		System.out.println("...");
-		System.out.println("Performance (millis): max: " + myCounter.getMax() + " mean: " + myCounter.getMean());
-		System.out.println("Palindromes computed: " + myCounter.listCount());
+		System.out.println("Performance (millis): max: " + myStats.getMax() + " mean: " + myStats.getMean());
+		System.out.println("Palindromes computed: " + myStats.getList().size());
 		System.out.println("Tasks run: ");
 		System.out.println("Duration: " + ((endTime - startTime)));				
 	}	
@@ -109,49 +124,21 @@ public class bigBucks {
 
 class Counter {
 
-    public Lock lock;
-    public static int count;
-    public static long meanTime;
-    public static long maxTime; 
-    public static boolean status = false; 
-	public static List<Integer> palindromeList = new ArrayList<Integer>();
+	private Lock lock;
+    private static long meanTime;
+    private static long maxTime; 
+    private static boolean status = false; 
+	private static List<Integer> palindromeList = new ArrayList<Integer>();
     
-    public Counter(Lock myLock) {
+	protected Counter(Lock myLock) {
         this.lock = myLock;
     }
-
-	public boolean checkStatus() {
-        lock.lock();
-        try {
-    		return status;
-        } finally {
-            lock.unlock();
-        }
-	}
 	
-	public void terminateThread() {
-        lock.lock();
-        try {
-    		status = true;
-        } finally {
-            lock.unlock();
-        }
-	}
-
-	public void unlock() {
+	protected void unlock() {
 		lock.unlock();
 	}
-
-	public final int getCount() {
-        lock.lock();
-        try {
-            return count;
-        } finally {
-            lock.unlock();
-        }
-    }
 	
-	public final void addList(int match) {
+	protected void addList(int match) {
 		lock.lock();
         try {
         	palindromeList.add(match);
@@ -160,57 +147,37 @@ class Counter {
         }
 	}
 	
-	public final List<Integer> getList() {
-		lock.lock();
-        try {
-        	return palindromeList;
-        } finally {
-            lock.unlock();
-        }
-	}
-	
-	public final int listCount() {
-		return palindromeList.size();
-	}
-	
-	
-	public final void addCount() {
-        lock.lock();
-        try {
-            count++;
-        } finally {
-            lock.unlock();
-        }
-    }
-	
-	public final void threadTime(long time) {
+	protected void threadTime(long time) {
         lock.lock();
         try {
             if (time > maxTime)
             	maxTime = time;
             if (meanTime == 0)
             	meanTime = time;
-            meanTime = (meanTime + time) / 2;
+            else
+            	meanTime = (meanTime + time) / 2;
         } finally {
             lock.unlock();
         }
     }
+
+	protected void terminateThread() {
+		status = true;
+	}
 	
-	public final long getMean() {
-        lock.lock();
-        try {
-            return meanTime;
-        } finally {
-            lock.unlock();
-        }
+	protected boolean checkStatus() {
+		return status;
+	}
+	
+	protected List<Integer> getList() {
+    	return palindromeList;
+	}
+	
+	protected long getMean() {
+        return meanTime;
     }
 	
-	public final long getMax() {
-        lock.lock();
-        try {
-            return maxTime;
-        } finally {
-            lock.unlock();
-        }
+	protected long getMax() {
+        return maxTime;
     }
 }
