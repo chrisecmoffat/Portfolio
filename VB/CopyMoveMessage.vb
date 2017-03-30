2de90d327a1d5514b1fcdfbf4c66e2362fe1a541
chrisecmoffat <chris.ec.moffat@gmail.com>
2017-03-30 19:14:52 -0400

    Update and rename VBA/Outlook/Inbox-Organization/CopyMoveMessage to VB/CopyMoveMessage.vb

--- VB/CopyMoveMessage.vb
+++ VB/CopyMoveMessage.vb
@@ -1,39 +1,36 @@
-Sub MoveCopyMessage()
-On Error GoTo ProcExit
+Sub CopyMoveMessage()
+    On Error GoTo ProcExit
   
     Dim objNS As Outlook.NameSpace
-    Dim objSourceFolder As Outlook.MAPIFolder
     Dim objDestFolder As Outlook.MAPIFolder
     Dim objDestFolder2 As Outlook.MAPIFolder
     Dim objItem As Object
     Dim objCopy As Object
     Set objNS = Application.GetNamespace("MAPI")
     Set Selection = ActiveExplorer.Selection
- 
-' Set the source and destination folders
-    Set objSourceFolder = objNS.Folders("Production Services")
+     
+    ' Set the destination folders
     Set objDestFolder = objNS.Folders("Production Services") _
-         .Folders("CLOSED Items").Folders("Chris")
+     .Folders("CLOSED Items").Folders("Chris")
     Set objDestFolder2 = objNS.Folders("Production Services") _
-         .Folders("CLOSED Items")
+     .Folders("CLOSED Items")
          
     Set objItem = Application.ActiveExplorer.Selection.Item(1)
+    
     For i = 1 To Selection.Count
         If i = 5 Then
             Exit For
         End If
         Set objItem = Selection(i)
-    
- ' copy and move first
+        
         Set objCopy = objItem.Copy
         objCopy.Move objDestFolder
         objItem.Move objDestFolder2
     Next
 
 ProcExit:
-  Exit Sub
+    Exit Sub
 
-    Set objSourceFolder = Nothing
     Set objDestFolder = Nothing
     Set objNS = Nothing
 End Sub
