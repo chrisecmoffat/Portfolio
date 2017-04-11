a88baaa9faf90e9ce2d50100deba160d372df71a
Chris Moffat <chris.ec.moffat@gmail.com>
2017-04-11 14:58:43 -0400

    Create BlackJack.js

---
+++ Java/BlackJack.js
@@ -0,0 +1,18 @@
+ <body>
+  
+	<h1>Black Jack<h1>
+
+    <h1>Dealer Card 1: <span id="p1Card1">0</span></h1> 
+    <h1>Dealer Card 2: <span id="p1Card2">0</span></h1>
+    <h1>Total: <span id="p1Total">0</span></h1>
+    <br></br>
+    <h1>Player Card 1: <span id="p2Card1">0</span></h1> 
+    <h1>Player Card 2: <span id="p2Card2">0</span></h1>
+    <h1>Total: <span id="p2Total">0</span></h1>
+ 
+    <button id="p1">Stay</button>
+    <button id="p2">Hit Me</button>
+    <button id="reset">Reset</button>
+  	<button id="random">Randomizer</button>
+		
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
