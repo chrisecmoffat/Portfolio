376c66b3e430ceefa3a60d97aba82565eeb24200
Chris Moffat <chris.ec.moffat@gmail.com>
2017-08-14 13:43:37 -0400

    Update CopyMoveMessage.vb

--- VB/CopyMoveMessage.vb
+++ VB/CopyMoveMessage.vb
@@ -1,36 +1,40 @@
-Sub CopyMoveMessage()
+Sub CopyMoveMessageV2()
     On Error GoTo ProcExit
-  
-    Dim objNS As Outlook.NameSpace
-    Dim objDestFolder As Outlook.MAPIFolder
-    Dim objDestFolder2 As Outlook.MAPIFolder
-    Dim objItem As Object
-    Dim objCopy As Object
-    Set objNS = Application.GetNamespace("MAPI")
-    Set Selection = ActiveExplorer.Selection
-     
-    ' Set the destination folders
-    Set objDestFolder = objNS.Folders("Production Services") _
-     .Folders("CLOSED Items").Folders("Chris")
-    Set objDestFolder2 = objNS.Folders("Production Services") _
-     .Folders("CLOSED Items")
-         
-    Set objItem = Application.ActiveExplorer.Selection.Item(1)
     
+    Dim folderName As String
+    folderName = "Chris" 'change this value to the name of the folder
+    
+    Set Selection = ActiveExplorer.Selection
+    Set objDestFolder1 = Application.GetNamespace("MAPI").Folders("ABCDE") _
+                        .Folders("CLOSED Items")
+    Set objDestFolder2 = Application.GetNamespace("MAPI").Folders("ABCDE") _
+                        .Folders("CLOSED Items").Folders(folderName)
+                Set objDestFolder3 = Application.GetNamespace("MAPI").Folders("ABCDE") _
+                        .Folders("CLOSED Items").Folders(folderName).Folders(folderName)
+                    
     For i = 1 To Selection.Count
+        DoEvents
         If i = 5 Then
             Exit For
         End If
-        Set objItem = Selection(i)
         
-        Set objCopy = objItem.Copy
-        objCopy.Move objDestFolder
-        objItem.Move objDestFolder2
+        Set objItem = Selection(i)
+        objItem.Move objDestFolder3
+    Next
+       
+    For i = objDestFolder3.Items.Count To 1 Step -1
+        DoEvents
+        Set currentItem = objDestFolder3.Items(i)
+        Set objCopy = currentItem.Copy
+        objCopy.Move objDestFolder2
+        currentItem.Move objDestFolder1
     Next
 
 ProcExit:
     Exit Sub
-
-    Set objDestFolder = Nothing
-    Set objNS = Nothing
+    
+    Set objDestFolder1 = Nothing
+    Set objDestFolder2 = Nothing
+    Set objDestFolder3 = Nothing
+   
 End Sub
